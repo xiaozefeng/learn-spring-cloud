e7a6fb0764a365778db3ff57790a6ccbfce4a886
肖泽锋 <qq523107430@163.com>
2018-03-10 21:29:49 +0800

    配置中心服务

--- order/server/src/main/java/com/immoc/order/controller/ClientController.java
+++ order/server/src/main/java/com/immoc/order/controller/ClientController.java
@@ -4,6 +4,8 @@ import com.immoc.product.client.ProductClient;
 import com.immoc.product.common.ProductInfoOutput;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +16,7 @@ import java.util.List;
  * @author xiaozefeng
  */
 @RestController
+@RefreshScope
 @Slf4j
 public class ClientController {
 
@@ -25,6 +28,14 @@ public class ClientController {
         List<ProductInfoOutput> productInfoList = productClient.listForOrder(Arrays.asList("157875196366160022"));
         log.info("response => {}", productInfoList);
         return "ok";
+    }
+
+    @Value("${env}")
+    private String env;
+
+    @GetMapping("env/print")
+    public String env(){
+        return env;
     }
 
 }
