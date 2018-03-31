f771e899641ecb02f43217bd967e82266c04c0d3
肖泽锋 <qq523107430@163.com>
2018-03-31 22:40:25 +0800

    添加hystrix-dashboard 依赖

--- order/server/src/main/java/com/immoc/order/OrderApplication.java
+++ order/server/src/main/java/com/immoc/order/OrderApplication.java
@@ -2,6 +2,7 @@ package com.immoc.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -17,6 +18,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringCloudApplication
 // 扫描 product client
 @ComponentScan(basePackages = {"com.immoc.order", "com.immoc.product.client"})
+@EnableHystrixDashboard
 public class OrderApplication {
 
     @Bean
